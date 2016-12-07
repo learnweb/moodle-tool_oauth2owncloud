3d600ffced7d90bd96da428cd0c03c12fe38aca6
walsidalw <kleinfachnull@gmail.com>
2016-12-07 12:05:55 +0100

    Edited the event observer.

--- classes/observer.php
+++ classes/observer.php
@@ -33,9 +33,7 @@ class observer {
 
         // Just a test for Events. The username and password of the login attempt to Sciebo
         // are bypassed to the admin tool and here they are written into the user preferences.
-        if ((get_user_preferences('webdav_user') == null) || (get_user_preferences('webdav_pass') == null)) {
-            set_user_preference('webdav_user', $event->other['user']);
-            set_user_preference('webdav_pass', $event->other['pass']);
-        }
+        set_user_preference('webdav_user', $event->other['user']);
+        set_user_preference('webdav_pass', $event->other['pass']);
     }
 }
