7601e3719510f6c091ffc7a46f875368cec85ea7
walsidalw <kleinfachnull@gmail.com>
2017-03-27 11:13:28 +0200

    Removed unused capability.

--- db/access.php
+++ db/access.php
@@ -25,13 +25,4 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$capabilities = array(
-    'tool/oauth2owncloud:editsettings' => array(
-        'riskbitmask' => RISK_CONFIG,
-        'captype' => 'write',
-        'contextlevel' => CONTEXT_SYSTEM,
-        'archetypes' => array(
-            'manager' => CAP_ALLOW
-        )
-    )
-);
+$capabilities = array();
